498c7b273a279e4ea0b0630c79c8737a8121312d
MezahirQedim <85835578+MezahirQedim@users.noreply.github.com>
2023-07-25 23:24:44 +0400

    get methodu ile api cekilmesi

--- src/Components/Body/Users.js
+++ src/Components/Body/Users.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
 const Users = () => {
+
+  const [users, setUsers] = React.useState([])
+
+
+  // https://dummy.restapiexample.com/api/v1/employees
+
+  // const loadData = () => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then(test =>  test.json())
+  //   .then(response => setUsers(response))
+  //   .catch(err => console.log(err))
+  // }
+
+  console.log(users)
+
+  const loadData2 = async () => {
+    	const users2 = await fetch("https://jsonplaceholder.typicode.com/users")
+
+      const test = await users2.json()
+
+      if(users2.status === 200 && users2.ok){
+          setUsers(test)
+      }else {
+
+      }
+
+  }
+
+
+  React.useEffect(() => {
+    loadData2()
+  },[])
+
+
   return (
-    <div>Users</div>
+    <div>
+      <button
+        // onClick={loadData}
+      >Data</button>
+    </div>
   )
 }
 
