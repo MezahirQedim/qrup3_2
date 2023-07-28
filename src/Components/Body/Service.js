2a8a9022aae2ec4f9a0845458c38579022ddcff3
MezahirQedim <85835578+MezahirQedim@users.noreply.github.com>
2023-07-28 18:19:32 +0400

    post methodu ile api-nin cekilmesi

--- src/Components/Body/Service.js
+++ src/Components/Body/Service.js
@@ -1,8 +1,55 @@
 import React from 'react'
 
 const Service = () => {
+
+
+  const [userId, setUserId] = React.useState("")
+  const [title, setTitle] = React.useState("")
+
+
+
+    const Add = () => {
+
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+
+        method: "POST",
+        body: JSON.stringify({
+          userId: userId,
+          title: title,
+        })
+
+      })
+    }
+
+
+    // React.useEffect(() => {
+    //   Add()
+    // }, [])
+
+
   return (
-    <div>Service</div>
+    <div>
+
+      title
+      <input onChange={(e) => setTitle(e.target.value)}/>
+
+      userId
+
+
+      <input onChange={(e) => setUserId(e.target.value)}/>
+
+      <button
+         
+         onClick={Add}
+
+      >
+        
+        Add
+        
+        </button>
+
+
+    </div>
   )
 }
 
--- src/Components/Body/Users.js
+++ src/Components/Body/Users.js
@@ -30,6 +30,9 @@ const Users = () => {
   }
 
 
+  
+
+
   React.useEffect(() => {
     loadData2()
   },[])
